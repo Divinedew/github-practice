a4b11e0097a52f68771835a0b406872c94f611db
Divinedew <egwimdivinedew@gmail.com>
2026-03-13 17:03:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const startLine = '     ||<- Start line';
+
+let turtle = "🐢";
+let rabbit= "🐇";
+
+let turtle2 = turtle.padStart(9, " ");
+let rabbit2 = rabbit.padStart(9, " ");
+
+let turtle3 = turtle.trim().padEnd(9, '=');
+
+
+let obj = {
+  my: 'name',
+  is: 'Rudolf',
+  the: 'reindeer'
+}
+
+let objTransform = Object.entries(obj).map(value =>
+    value.join(" ")
+)
+
+let objTransform2 = objTransform.join(" ");
+
+
+console.log (startLine);
+console.log(turtle2);
+console.log (rabbit2);
+console.log(turtle3);
+console.log(objTransform2);*/
